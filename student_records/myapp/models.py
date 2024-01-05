48fe80beb95a685fcbd6833da02be0ddd1dce7a6
mujeeb-gh <olamidebalogun174@gmail.com>
2024-01-05 17:20:02 +0100

    added update & delete functionality

--- student_records/myapp/models.py
+++ student_records/myapp/models.py
@@ -4,7 +4,8 @@ from django.db import models
 from django.db import models
 
 class Student(models.Model):
-    matric_number = models.CharField(primary_key=True, max_length=50)
+    id = models.AutoField(primary_key=True)
+    matric_number = models.CharField(unique=True, max_length=20)
     full_name = models.CharField(max_length=255)
     passport_photo = models.ImageField(upload_to='passport_photos/', null=True, blank=True)
     department = models.CharField(max_length=100)
