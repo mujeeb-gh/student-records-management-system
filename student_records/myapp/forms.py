fe6e168473983e1d231d8ad8482923d5ec7b2493
mujeeb-gh <olamidebalogun174@gmail.com>
2024-01-07 16:12:16 +0100

    feat: Add DOB DatePicker functionality

--- student_records/myapp/forms.py
+++ student_records/myapp/forms.py
@@ -1,7 +1,13 @@
 from django import forms
 from .models import Student
+from bootstrap_datepicker_plus.widgets import DatePickerInput
+
 
 class StudentForm(forms.ModelForm):
   class Meta:
       model = Student
       fields = ['matric_number', 'full_name', 'passport_photo', 'department', 'college', 'cgpa', 'gender', 'age', 'date_of_birth', 'contact_no', 'address', 'email', 'nationality', 'state']
+      
+      widgets = {
+            'date_of_birth': DatePickerInput(),
+        }
--- student_records/student_records/settings.py
+++ student_records/student_records/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myapp'
+    'myapp',
+    'bootstrap4',
+    "bootstrap_datepicker_plus",
 ]
 
 MIDDLEWARE = [
